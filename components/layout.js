3d6cfde49981c3d549a4d43ab8ba422b86089e94
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2022-07-17 18:26:12 -0400

    add search bar

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,19 @@
 // import styles from './layout.module.css';
 import icon from '../public/ei_icon.png';
 import Image from 'next/image';
-import { chakra, Text, Box, LinkBox, Flex, Heading, Spacer, HStack, Button, LinkOverlay, useMediaQuery, IconButton } from '@chakra-ui/react';
+import { chakra, Input, Text, Box, LinkBox, Flex, Heading, Spacer, HStack, Button, LinkOverlay, useMediaQuery, useDisclosure, IconButton } from '@chakra-ui/react';
 import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons'
 import NextLink from 'next/link'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
 export default function Layout({ children }) {
   const height = 100;
@@ -15,6 +25,8 @@ export default function Layout({ children }) {
 
   const [isDesktop] = useMediaQuery('(min-width: 768px)')
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <>
     <Flex minWidth='max-content' align='center' gap='4' p='4'>
@@ -25,17 +37,39 @@ export default function Layout({ children }) {
       </LinkBox>
 
       <Box p='2'>
-        <Heading size='md'>Express Immigration</Heading>
+        <Heading size='md' color='#135caa' display='inline-block' pr={1}>EXPRESS</Heading>
+        <Heading size='md' color='#de0d24' display='inline-block' fontWeight='normal'>IMMIGRATION</Heading>
       </Box>
 
       <Spacer/>
 
-      <Button display={isDesktop ? 'flex' : 'none'} justifyContent='start'>
+      <Button display={isDesktop ? 'flex' : 'none'} justifyContent='start' onClick={onOpen}>
         <HStack>
           <SearchIcon color='gray'/>
           <Text fontWeight='light' color='gray'>Search the knowledge base</Text>
         </HStack>
       </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          {/* <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton /> */}
+          <ModalBody p={0}>
+            <Flex align='center'>
+              <SearchIcon m={6}/>
+              <Input h='70px' p={0} border={0} placeholder='Search' />
+            </Flex>
+          </ModalBody>
+
+          {/* <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter> */}
+        </ModalContent>
+      </Modal>
 
       <Spacer/>
       
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import NextLink from 'next/link'
 import Image from 'next/image'
 import icon from '../public/ei_icon.png'
-import { Flex, Spacer, Grid, GridItem, Button, Text, Box, chakra, HStack, useMediaQuery } from '@chakra-ui/react'
+import { Flex, Heading, Grid, GridItem, Button, Text, Box, chakra, HStack, useMediaQuery, useDisclosure, Input } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
 export default function Home() {
   const NextImage = chakra(Image, {
@@ -11,6 +21,8 @@ export default function Home() {
 
   const [isDesktop] = useMediaQuery('(min-width: 768px)')
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <Flex direction="column" align="center" justify="center" height="100vh">
 
@@ -18,11 +30,13 @@ export default function Home() {
         p={5}
         templateAreas={isDesktop ? 
         ` ".    logo    logo    ."
+          ".    title   title   ."
           ".    srch    srch    ."
           "sp   ee      v       o"` :
 
         ` "logo   logo"
           "srch   srch"
+          "title  title"
           " sp    ee  "
           " v     o   "`}
         templateColumns={isDesktop ? 'repeat(4, 1fr)' : 'repeat(2, 1fr)'}
@@ -35,13 +49,39 @@ export default function Home() {
           </Box>
         </GridItem>
 
+        <GridItem area='title'>
+          <Heading size='md' color='#135caa' display='inline-block' pr={1}>EXPRESS</Heading>
+          <Heading size='md' color='#de0d24' display='inline-block' fontWeight='normal'>IMMIGRATION</Heading>
+        </GridItem>
+
         <GridItem area='srch'>
-          <Button w={isDesktop ? '80%' : '100%'} justifyContent='start'>
+          <Button w={isDesktop ? '80%' : '100%'} justifyContent='start' onClick={onOpen}>
             <HStack>
               <SearchIcon color='gray'/>
               <Text fontWeight='light' color='gray'>Search the knowledge base</Text>
             </HStack>
           </Button>
+
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              {/* <ModalHeader>Modal Title</ModalHeader>
+              <ModalCloseButton /> */}
+              <ModalBody p={0}>
+                <Flex align='center'>
+                  <SearchIcon m={6}/>
+                  <Input h='70px' p={0} border={0} placeholder='Search' />
+                </Flex>
+              </ModalBody>
+
+              {/* <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button variant='ghost'>Secondary Action</Button>
+              </ModalFooter> */}
+            </ModalContent>
+          </Modal>
         </GridItem>
 
         <GridItem area='sp'>
